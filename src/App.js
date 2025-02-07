4647c79fd9b9582d02de179e7f7767774bb0ef6f
SuryaNarayanan S <144770284+SuryaNarayananDev@users.noreply.github.com>
2025-02-07 09:58:16 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -22,8 +22,3 @@ function App() {
 }
 
 export default App;
-//https://ca-times.brightspotcdn.com/dims4/default/087112f/2147483647/strip/true/crop/5906x3937+0+0/resize/1200x800!/format/webp/quality/75/?url=https%3A%2F%2Fcalifornia-times-brightspot.s3.amazonaws.com%2F58%2F4e%2Fa59edfeb4c00b85217973e5560f6%2Fsquidgame-unit-104-442.jpg
-
-//https://i.kinja-img.com/image/upload/c_fit,q_60,w_1315/d264d3c293494a33c5dbc15d221ba966.jpg
-
-//https://assetsio.gnwcdn.com/squid-game-board-game-artwork.jpg?width=970&quality=80&format=jpg&dpr=2&auto=webp
